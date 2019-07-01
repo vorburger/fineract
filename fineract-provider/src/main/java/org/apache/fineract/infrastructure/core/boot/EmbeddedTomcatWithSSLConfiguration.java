db43755e860582c1e19b4bd4bc021a762fe55a19
Michael Vorburger.ch <mike@vorburger.ch>
2019-07-01 21:58:06 +0200

    re-activate EmbeddedTomcatWithSSLConfiguration (re. FINERACT-764)
    It only didn't build anymore because the Commons IO was removed some time
    (perhaps due to a version conflict?); but we don't really need it, it seems
    to have been created for an obscure file path length problem on Windows?
    
    If that Windows problem comes back, then we can always revert this, and properly fix.
    (I may have actually coded that myself, many many years ago, it feels familiar.)
    For now, this unblocks the FINERACT-764, req. for Gradle upgrade FINERACT-700.
    
    Also delete EmbeddedTomcatWithSSLConfigurationTest, because it really
    only tested that funky (and now deleted) resource extraction from JAR,
    not really the EmbeddedTomcatWithSSLConfigurationTest.

--- fineract-provider/src/main/java/org/apache/fineract/infrastructure/core/boot/EmbeddedTomcatWithSSLConfiguration.java
+++ fineract-provider/src/main/java/org/apache/fineract/infrastructure/core/boot/EmbeddedTomcatWithSSLConfiguration.java
@@ -23,7 +23,6 @@ import java.io.IOException;
 import java.net.URL;
 
 import org.apache.catalina.connector.Connector;
-import org.apache.commons.io.FileUtils;
 import org.apache.coyote.http11.Http11NioProtocol;
 import org.springframework.boot.context.embedded.EmbeddedServletContainerFactory;
 import org.springframework.boot.context.embedded.tomcat.TomcatEmbeddedServletContainerFactory;
@@ -53,7 +52,7 @@ public class EmbeddedTomcatWithSSLConfiguration {
         Connector connector = new Connector("org.apache.coyote.http11.Http11NioProtocol");
         Http11NioProtocol protocol = (Http11NioProtocol) connector.getProtocolHandler();
         try {
-            File keystore = getFile(getKeystore());
+            File keystore = getKeystore().getFile();
             File truststore = keystore;
             connector.setScheme("https");
             connector.setSecure(true);
@@ -81,40 +80,5 @@ public class EmbeddedTomcatWithSSLConfiguration {
 
     protected Resource getKeystore() {
         return new ClassPathResource("/keystore.jks");
-    }
-
-    public File getFile(Resource resource) throws IOException {
-        try {
-            return resource.getFile();
-        } catch (IOException e) {
-            // Uops.. OK, try again (below)
-        }
-
-        try {
-            URL url = resource.getURL();
-            /**
-             * // If this creates filenames that are too long on Win, // then
-             * could just use resource.getFilename(), // even though not unique,
-             * real risk prob. min.bon String tempDir =
-             * System.getProperty("java.io.tmpdir"); tempDir = tempDir + "/" +
-             * getClass().getSimpleName() + "/"; String path = url.getPath();
-             * String uniqName = path.replace("file:/", "").replace('!', '_');
-             * String tempFullPath = tempDir + uniqName;
-             **/
-            // instead of File.createTempFile(prefix?, suffix?);
-            File targetFile = new File(resource.getFilename());
-            long len = resource.contentLength();
-            if (!targetFile.exists() || targetFile.length() != len) { // Only
-                                                                      // copy
-                                                                      // new
-                                                                      // files
-                FileUtils.copyURLToFile(url, targetFile);
-            }
-            return targetFile;
-        } catch (IOException e) {
-            // Uops.. erm, give up:
-            throw new IOException("Cannot obtain a File for Resource: " + resource.toString(), e);
-        }
-
     }
 }
